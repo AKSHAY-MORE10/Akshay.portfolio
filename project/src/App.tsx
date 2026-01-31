3c7987f7f827a9462861d001263a18e8c66bccff
Akshay More <akshaybapumore@gmail.com>
2026-01-31 17:10:14 +0530

    a

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
         <SplashCursor />
         <div className="relative z-10">
           <NavBar items={navItems} className='h-fit'/>
-          <main className="pt-20">
+          <main className="pt-15">
             <Hero/>
             <AboutSection />
             <div className="w-full px-4">
